6dfd32fe28c463d2799e21757a008a5a4131c5da
RedWatcher23 <157664591+RedWatcher23@users.noreply.github.com>
2024-05-14 13:00:21 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+FILEPATH = "todos.txt"
 
-This is a temporary script file.
-"""
 
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
